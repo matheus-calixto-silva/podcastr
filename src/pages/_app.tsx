43028734942af5aa1ec04f4962c7020208856939
Matheus Calixto <matheuscalixto8@gmail.com>
2021-04-23 03:34:49 -0300

    Implementação da reprodução de áudios

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,30 @@ import "../../styles/global.scss";
 import { Header } from "../components/Header/header";
 import { Player } from "../components/Player/player";
 import styles from "../../styles/app.module.scss";
+import React, { useState } from "react";
+import { PlayerContext } from "../contexts/PlayerContext";
 
 function MyApp({ Component, pageProps }) {
+  const [episodeList, setEpisodeList] = useState([]);
+  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  function play(episode){
+    setEpisodeList([episode]);
+    setCurrentEpisodeIndex(0);
+    setIsPlaying(true);
+  }
+
+  function togglePlay () {
+    setIsPlaying(!isPlaying);
+  }
+
+  function setPlayingState(state: boolean){
+    setIsPlaying(state);
+  }
+
   return (
+    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, play, isPlaying, togglePlay, setPlayingState }}>
     <div className={styles.wrapper}>
       <main>
         <Header />
@@ -12,6 +33,7 @@ function MyApp({ Component, pageProps }) {
       </main>
       <Player />
     </div>
+    </PlayerContext.Provider>
   );
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import { PlayerContext } from '../contexts/PlayerContext';
+import { useContext } from "react";
 import { GetStaticProps } from "next";
 import Image from "next/image";
 import Link from "next/link";
@@ -25,6 +27,8 @@ type HomeProps = {
 };
 
 export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
+  const {play} = useContext(PlayerContext);
+
   return (
     <div className={styles.homepage}>
       <section className={styles.latestEpisodes}>
@@ -50,7 +54,7 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
                   <span>{episode.durationAsString}</span>
                 </div>
 
-                <button type="button">
+                <button type="button" onClick={() => play(episode)}>
                   <img src="/play-green.svg" alt="Tocar episódio" />
                 </button>
               </li>
